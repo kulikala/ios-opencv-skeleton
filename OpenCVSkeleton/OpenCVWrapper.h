6a424cce70b75014a5380981b5de0100bb3f6bf2
Kaz Namba <kaz.namba@gmail.com>
2019-10-21 17:51:58 +0900

    feat: Create empty wrapper for OpenCV

---
+++ OpenCVSkeleton/OpenCVWrapper.h
@@ -0,0 +1,23 @@
+//
+//  OpenCVWrapper.h
+//  OpenCVSkeleton
+//
+//  Created by Kaz Namba on 2019/10/21.
+//  Copyright © 2019 Kaz Namba. All rights reserved.
+//
+
+#ifndef OpenCVWrapper_h
+#define OpenCVWrapper_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+/**
+ Objective-C implementations using OpenCV
+ */
+@interface OpenCVWrapper : NSObject
+
+@end
+
+#endif /* OpenCVWrapper_h */
---
+++ OpenCVSkeleton/OpenCVWrapper.mm
@@ -0,0 +1,29 @@
+//
+//  OpenCVWrapper.mm
+//  OpenCVSkeleton
+//
+//  Created by Kaz Namba on 2019/10/21.
+//  Copyright © 2019 Kaz Namba. All rights reserved.
+//
+
+
+#import <opencv2/opencv.hpp>
+
+#import "OpenCVWrapper.h"
+
+using namespace cv;
+using namespace std;
+
+
+/**
+ Objective-C implementations using OpenCV
+ */
+@interface OpenCVWrapper() {
+}
+
+@end
+
+
+@implementation OpenCVWrapper
+
+@end
