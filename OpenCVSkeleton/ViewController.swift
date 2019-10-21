0004678f55efafbd7eb75b3d055c0d245ac02ec6
Kaz Namba <kaz.namba@gmail.com>
2019-10-21 17:35:29 +0900

    Initial commit with template Xcode project

---
+++ OpenCVSkeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCVSkeleton
+//
+//  Created by Kaz Namba on 2019/10/21.
+//  Copyright © 2019 Kaz Namba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
